98186c8477b210e6196d14294d24a67a15f265a8
Patrick <patrick.souza96@outlook.com>
2019-03-01 14:08:53 -0300

    eslint

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# react Native Template
+# React Native Template
 Template react native with typescript, jest and react-navigation
+
+#Use
+react-native init MyProject --template tegra-template --jest-skip
---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+export const env = __DEV__ ? 'development' : 'production';
+export const isDevelopment = env === 'development';
+
+export const apiTimeout = 15 * 1000;
+
+export const endPoint = 'https://localhost:3000/';
